d2d2291a16f26b2d1d4167d61da3fb1a275ad1fd
alessio0728 <147940140+alessio0728@users.noreply.github.com>
2023-11-16 21:54:20 +0100

    Add files via upload

---
+++ esercitazione del 16-11-2023.js
@@ -0,0 +1,47 @@
+const array1 = ["Alessio" , "Umberto", "Angelo" ,"Roberta", "Viola"]
+    
+for (const element of array1) {
+
+    console.log (element);
+}
+
+let array2 = ["Alessio" ,"Umberto","Angelo" ,"Roberta", "Viola"]
+
+for (let i = 0; i < array2.length; i++) {
+
+    for(let j = 0 ;j < array2[i].length;j++){
+
+    console.log(array2[i][j])
+
+}
+
+  }
+
+  //terza traccia//
+
+// Quarta traccia//
+
+    let a = 1;
+
+    let b = 1;
+
+    let somma = 0
+
+    for (let i = 1; i<=200; i++){
+
+        somma = somma + i;
+    }
+
+    console.log(somma)
+
+    //Quinta traccia//
+
+    for (let z = 1 ; z <- 400 ; z++){
+
+        if(z % 20 == 0){
+
+            console.log(z)
+        }
+    }
+        
+    
